4888f72a092ac70bd8c9060ed34606e9efae6633
Daniel Rapp <danielrappt@gmail.com>
2014-01-16 17:33:31 +0100

    Added input through stdin

--- 68k.js
+++ 68k.js
@@ -4,3 +4,13 @@ var registers = {
   d1: 0
 };
 
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function (chunk) {
+  process.stdout.write('data: ' + chunk);
+});
+
+process.stdin.on('end', function () {
+  process.stdout.write('end');
+});
